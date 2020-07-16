23082636f0d78f0ddd1c843d2ad56d8c4bef3ab0
Ryan Wright <ryanwright1992@gmail.com>
2020-07-16 21:48:40 +0100

    Uploading 1st daily python challenge

---
+++ python1.py
@@ -0,0 +1,14 @@
+#Python Write a python script with the above name which does the following:
+
+
+	
+#initialises a list which contains the FIRST NAME of all 21 people in this cohort
+	#using the .append() function add me to the list
+	#print the 5th person in the list
+	#print the number of Chris in the cohort
+
+july_cohort = ['Christopher','John','Jason','Arsalan','Domenico','Joshua','Ryan','Bradley','Javas','Sithembiso','Jacob','Clifford','Wasim','Mohamed','Tobias','Christopher','Samuel','Edmund','Amanda','Steven','Diarmuid']
+
+july_cohort.append('luke')
+print(july_cohort[4])
+print(july_cohort.count('Christopher'))
